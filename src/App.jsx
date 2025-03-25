1d6bd77abdbd72ab508f2a2db00fc6fbd9931d06
Akash Biswal <akashbiswaliit7@gmail.com>
2025-03-25 17:27:34 +0530

    Components jsx files Created

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter } from 'react-router-dom';
+import {
+    About, Contact, Experience, Feedback, Hero, Navbar, Tech, Works, StarCanvas
+} from './components';
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+
+        <div className='relative z-0 bg-primary'>
+            <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                <Navbar />
+                <Hero />
+            </div>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+            <Feedbacks />
+            <div className='relative z-0'>
+                <Contact />
+                <StarsCanvas />
+            </div>
+        </div>
+
+       </BrowserRouter>
+    )
+}
+
+export default App
